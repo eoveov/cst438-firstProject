ffd996d6a890dcaa35f3d4db8d7abe876448e1a2
Eric Orozco Viscarra <onlyforeric123@gmail.com>
2019-09-10 19:01:05 -0700

    adding services and bindings

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   myMethod() {
-    return console.log('hey, what is up!');
+    return console.log('Hey, what is up!');
+  }
+
+  getBeer() {
+    return this.http.get('https://api.openbrewerydb.org/breweries');
   }
 }
