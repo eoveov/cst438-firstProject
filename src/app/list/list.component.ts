4ec005eeb2983a8dcd2bfd05db788d67668edeea
Eric Orozco Viscarra <onlyforeric123@gmail.com>
2019-09-10 17:27:12 -0700

    initial commit after mistake

---
+++ src/app/list/list.component.ts
@@ -0,0 +1,17 @@
+import { HttpService  } from './../http.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent implements OnInit {
+
+  constructor(private _http: HttpService) { }
+
+  ngOnInit() {
+    this._http.myMethod();
+  }
+
+}
